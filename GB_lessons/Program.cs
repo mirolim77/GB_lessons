d612ace61c21a1d2fa3b6f464001f524df03786a
Admin <Admin@DESKTOP-SG39KPJ>
2021-07-26 01:06:16 +0500

    Добавьте файлы проекта.

---
+++ GB_lessons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GB_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
