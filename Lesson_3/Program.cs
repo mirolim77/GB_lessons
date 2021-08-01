a24aac66b97ce2fd2eec2f64853caa41ab34c971
mirolim77 <mirolimjon@gmail.com>
2021-08-01 21:47:05 +0500

    lesson 3

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -6,10 +6,43 @@ namespace Lesson_3
     {
         static void Main(string[] args)
         {
-            int[,] arrayy = new int[2, 2];
-            arrayy ={ 1,2},{3,3}
+            Random rd = new Random();
+            int[,] arrayy = new int[2, 4];
 
-            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i <2; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    arrayy[i, j] = rd.Next(10, 55);
+                }
+             }
+
+            for (int i = 0; i < arrayy.GetLength(0); i++)
+            {
+                for (int j = 0; j <arrayy.GetLength(1); j++)
+                {
+                    Console.Write( "array[" + i +"]["+j+"]= "+ arrayy[i, j] +";  ");
+                }
+            }
+
+            //end Lesson 3. task1
+
+
+            //start Lesson 3. task 2
+
+            string[,] phonebook = new string[5,2];
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
