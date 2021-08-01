1f5ae4aacc7ba36a6cf53d477ea4c9e700672621
mirolim77 <mirolimjon@gmail.com>
2021-08-01 23:57:32 +0500

    task 3 end

--- Lesson_3/Program.cs
+++ Lesson_3/Program.cs
@@ -7,35 +7,69 @@ namespace Lesson_3
         static void Main(string[] args)
         {
             Random rd = new Random();
-            int[,] arrayy = new int[2, 4];
+            int[,] arrayy = new int[2, 8];
 
-
-            for (int i = 0; i <2; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    arrayy[i, j] = rd.Next(10, 55);
-                }
-             }
 
             for (int i = 0; i < arrayy.GetLength(0); i++)
             {
-                for (int j = 0; j <arrayy.GetLength(1); j++)
+                for (int j = 0; j < arrayy.GetLength(1); j++)
                 {
-                    Console.Write( "array[" + i +"]["+j+"]= "+ arrayy[i, j] +";  ");
+                    arrayy[i, j] = rd.Next(10, 55);
                 }
             }
 
-            //end Lesson 3. task1
+            for (int i = 0; i < arrayy.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayy.GetLength(1); j++)
+                {
+                    Console.Write("array[" + i + "][" + j + "]= " + arrayy[i, j] + ";  ");
+                }
+            }
+            Console.WriteLine('\n');
+            //end   task1
 
 
-            //start Lesson 3. task 2
+            //start   task 2
 
-            string[,] phonebook = new string[5,2];
+            //string[,] phonebook = new string[5, 3];
 
+            //for (int i = 0; i < phonebook.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < phonebook.GetLength(1); j++)
+            //    {
+            //        switch (j)
+            //        {
+            //            case 0: Console.Write("input Name: "); phonebook[i, j] = Console.ReadLine(); break;
+            //            case 1: Console.Write("input phone number: "); phonebook[i, j] = Console.ReadLine(); break;
+            //            case 2: Console.Write("input mail: "); phonebook[i, j] = Console.ReadLine(); break;
+            //            default:
+            //                break;
+            //        }
+            //    }
+            //}
+            //Console.WriteLine();
 
+            //string person = ""; 
+            //for (int i = 0; i < phonebook.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < phonebook.GetLength(1); j++)
+            //    {
+            //        person += phonebook[i, j]+ "    ";
+            //    }
+            //    Console.Write("person: "+i+"  "+person + '\n');
+            //    person = "";
+            //}
+            //end task 2
 
-
+            //start task 3
+            string tekst = "";
+            tekst =  Console.ReadLine();
+            for (int i= tekst.Length;  i > 0; i--)
+            {
+                //var t = tekst[i-1];
+                Console.Write(tekst[i-1]);
+            }
+            // end task 3
 
 
 
