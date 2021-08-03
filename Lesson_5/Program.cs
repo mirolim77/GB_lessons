cc0415a1a305b064f1fc1c4f5c5fa3b403fc16bd
Mirolim Mirkhusanov <mirolimmir@live.com>
2021-08-03 13:11:04 +0500

    lesson 5, task 3

---
+++ Lesson_5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Lesson_5
+{
+    class Program
+    {
+       
+
+
+
+
+        static void Main(string[] args)
+        {
+           
+            //start task 1            
+            File.WriteAllText("text.txt", Console.ReadLine()); // записываем в файл строку
+            //end task 1
+
+            //start task 2
+            File.WriteAllText("startup.txt", DateTime.Now.ToString());
+            //end task2
+
+            //start task 3
+            string input_binary=Console.ReadLine();
+            byte[] binary_array = new byte[input_binary.Length];
+
+            byte[] array = { 1, 2, 3, 5, 7, 9, 11 };
+            foreach (var item in input_binary)
+            {
+                if(Char.IsDigit(item))
+                {
+                    binary_array.Append(Convert.ToByte(item));
+                }
+            }
+            File.WriteAllBytes("bytes.bin", binary_array);
+           //end task 3
+
+
+ 
+
+        }
+        
+
+       
+    }
+}
