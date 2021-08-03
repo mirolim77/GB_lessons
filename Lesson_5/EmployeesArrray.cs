0b6eae60ce2cebdb0df8bc0de2f94e20a35a1a30
Mirolim Mirkhusanov <mirolimmir@live.com>
2021-08-03 18:01:13 +0500

    clear

--- Lesson_5/EmployeesArrray.cs
+++ Lesson_5/EmployeesArrray.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson_5
 {
     class EmployeesArrray
     {
-         Employees[] employees_;
+        Employees[] employees_;
 
 
         public EmployeesArrray()
@@ -20,17 +16,17 @@ namespace Lesson_5
 
         public void SortEmployersByAgeToConsole(Employees[] _personals, int _age)
         {
-            Employees[] e = new Employees[_personals.Length];
+
             Console.WriteLine("Start sortable method by age over: " + _age);
             foreach (var item in _personals)
             {
                 if (item.GetEmploerAge() > 40)
                 {
-                    //e.Append(item);
+
                     item.GetEmployersToConsole();
                 }
             }
-           
+
         }
 
 
