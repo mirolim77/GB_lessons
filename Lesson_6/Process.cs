8adc6a1014f7b0f317d5a18fe29a6e9fcf2f21be
mirolim77 <mirolimjon@gmail.com>
2021-08-05 23:11:39 +0500

    lesson 6

---
+++ Lesson_6/Process.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6
+{
+    class Process
+    {
+        string ProcessName;
+        int ProcessID;
+        int ProcessUseMemory;
+    }
+}
