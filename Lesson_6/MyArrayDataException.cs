8bf5b4e3c10863b1119277eb04412cffa87f59cc
mirolim77 <mirolimjon@gmail.com>
2021-08-08 20:12:14 +0500

    ok

---
+++ Lesson_6/MyArrayDataException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Lesson_6
+{
+    public enum ErrorCodes
+    {
+        ItIsNotInt,
+        NoBuilding,
+    }
+
+
+    [Serializable]
+    class MyArrayDataException: Exception
+    {
+        
+        public ErrorCodes Code { get; }
+
+
+
+
+
+        public MyArrayDataException(ErrorCodes code)
+        {
+            Code = code;
+        }
+
+
+
+
+
+        static int CheckToInter(char symbil)
+        {
+            if (Char.IsDigit(symbil))
+            {
+                return symbil;
+            }
+            throw new Exception();
+        }
+
+    }
+}
